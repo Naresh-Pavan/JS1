ea8b44a9fc955a5310a68585c423ef6c188a5e74
Naresh-Pavan <71119630+Naresh-Pavan@users.noreply.github.com>
2022-03-17 21:46:30 +0530

    completed

---
+++ first.js
@@ -0,0 +1,14 @@
+var num=6;
+var flo=5.5;
+var str="Naresh";
+var boo=2>3;
+console.log("Number -",num)
+console.log("Float -",flo)
+console.log("String -",str)
+console.log("Boolean -",boo)
+let r=4;
+const pi=3.14;
+var area=pi*(r**2);
+var cir=2*pi*r;
+console.log("Area of the circle with radius 5 is",area);
+console.log("Circumference of the circle with radius 5 is", cir);
